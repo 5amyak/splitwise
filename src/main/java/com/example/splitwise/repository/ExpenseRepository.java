f2fc670c874cf6d888cafe2d28546b209ebeb644
samyak.jain2 <samyak.jain2@myntra.com>
2022-08-21 21:23:59 +0530

    Improved query

--- src/main/java/com/example/splitwise/repository/ExpenseRepository.java
+++ src/main/java/com/example/splitwise/repository/ExpenseRepository.java
@@ -11,14 +11,14 @@ import java.util.List;
 @Repository
 public interface ExpenseRepository extends CrudRepository<Expense, Long> {
 
-    @Query(value = "SELECT e.* " +
+    @Query(value = "SELECT DISTINCT e.* " +
             "FROM expenses e, transactions t " +
             "WHERE t.expense_id = e.expense_id AND e.user_group_id=?2 " +
             "   AND (t.from_user_id=?1 OR t.to_user_id=?1)",
             nativeQuery = true)
     List<Expense> findExpensesByUserGroup(Long userId, Long userGroupId);
 
-    @Query(value = "SELECT e.* " +
+    @Query(value = "SELECT DISTINCT e.* " +
             "FROM expenses e, transactions t " +
             "WHERE t.expense_id = e.expense_id AND e.date>?2 AND e.date<?3 " +
             "   AND (t.from_user_id=?1 OR t.to_user_id=?1)",
